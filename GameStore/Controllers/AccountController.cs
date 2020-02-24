b8930967f9f2fd9ef40b6aa38782747801b8a63f
Karonuk <losha.pisnyak42@gmail.com>
2020-02-24 20:45:39 +0200

    login

--- GameStore/Controllers/AccountController.cs
+++ GameStore/Controllers/AccountController.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using DAL;
+using DAL.Entities;
 using DAL.IdentityModel;
+using GameStore.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Controllers
 {
     public class AccountController : Controller
     {
         private readonly SignInManager<DbUser> _signInManager;
-        public AccountController(SignInManager<DbUser> signInManager)
+        private EFDbContext _context;
+        public AccountController(SignInManager<DbUser> signInManager,EFDbContext context)
         {
             _signInManager = signInManager;
         }
@@ -27,8 +32,24 @@ namespace GameStore.Controllers
         {
             return View();
         }
+
+        public Task Login(LoginViewModel user)
+        {
+            var User = _context.Users.FirstOrDefault(x=>x.User.Email==user.Email);
+            if (User != null)
+            {
+                var result = _signInManager.PasswordSignInAsync(User.User, user.Password, false, false).Result;
+                if (result.Succeeded)
+                {
+                    Authenticate(User.User.Email);                    
+                }
+            }
+            return RedirectToAction("Login","Home");
+        }
+
         private async Task Authenticate(string userName)
         {
+            
             // создаем один claim
             var claims = new List<Claim>
             {
